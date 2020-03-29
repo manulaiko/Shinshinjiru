8e355eafa81b13c139b64716c96725857274eeb1
manulaiko <manulaiko@gmail.com>
2020-03-29 17:20:48 +0200

    Initial commit

---
+++ src/main/java/com/manulaiko/atarashiinjiru/AtarashiinjiruApplication.java
@@ -0,0 +1,13 @@
+package com.manulaiko.atarashiinjiru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtarashiinjiruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtarashiinjiruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manulaiko/atarashiinjiru/AtarashiinjiruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manulaiko.atarashiinjiru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtarashiinjiruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
