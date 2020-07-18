8a84c4903baf3a39f12284f7e51e485e3241bd61
manulaiko <manulaiko@gmail.com>
2020-07-18 20:19:19 +0200

    Add library path selection

--- src/main/java/com/manulaiko/shinshinjiru/presenter/Settings.java
+++ src/main/java/com/manulaiko/shinshinjiru/presenter/Settings.java
@@ -5,17 +5,22 @@ import com.manulaiko.shinshinjiru.api.APIService;
 import com.manulaiko.shinshinjiru.api.event.InitUserEvent;
 import com.manulaiko.shinshinjiru.api.event.UserListsInitializedEvent;
 import com.manulaiko.shinshinjiru.view.event.ConfirmAlertEvent;
+import com.manulaiko.shinshinjiru.view.event.ErrorAlertEvent;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.configuration2.FileBasedConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import javafx.scene.image.ImageView;
+
+import java.io.File;
 
 /**
  * Settings presenter.
@@ -44,10 +49,14 @@ public class Settings {
     private Label syncDate;
 
     @FXML
+    private Label libraryPath;
+
+    @FXML
     public void initialize() {
         userName.setText(api.getUser().getName());
         userAvatar.setImage(new Image(api.getUser().getAvatar().getMedium()));
         syncDate.setText(config.getString("app.lastSync"));
+        libraryPath.setText(config.getString("app.libraryPath"));
     }
 
     @FXML
@@ -62,6 +71,22 @@ public class Settings {
         ShinshinjiruApplication.publish(new InitUserEvent(this));
     }
 
+    @FXML
+    public void onLibraryButton(ActionEvent actionEvent) {
+        var dc = new DirectoryChooser();
+        dc.setTitle("Choose the Media Library directory.");
+        dc.setInitialDirectory(new File(config.getString("app.libraryPath")));
+        var path = dc.showDialog(new Stage());
+
+        if (path == null) {
+            ShinshinjiruApplication.publish(new ErrorAlertEvent(this, "You need to select the path to the media library."));
+
+            return;
+        }
+
+        config.setProperty("app.libraryPath", path.getAbsolutePath());
+        libraryPath.setText(config.getString("app.libraryPath"));
+    }
 
     /**
      * Update last sync date.
